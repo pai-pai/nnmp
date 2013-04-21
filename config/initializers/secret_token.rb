2602d2e31e59b6fefe75ce3151b0a803141e3f57
pai-pai <paimai.dororo@gmail.com>
2013-04-21 21:00:52 +0400

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nnmp::Application.config.secret_token = 'ca354b601facf21b0b16c226ded07288d5acb64724ef64d55b3b40b2f3a35ea8fae4d2aa75a88abd67280e5882c3272ea6e1947a3276dce6ed25ac86fbcdb519'
