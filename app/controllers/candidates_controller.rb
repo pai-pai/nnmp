5922e389f94acaffc2554b41987dfd8e4abfc1b2
pai-pai <paimai.dororo@gmail.com>
2013-05-17 14:55:58 +0400

    Session :layout bug

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -41,7 +41,7 @@ class CandidatesController < ApplicationController
     end
 
     def show
-        session[:layout].clear
+        session[:layout] = nil
         @candidate = Candidate.find(params[:id])
         @title = @candidate.fam_name + " " + @candidate.first_name + " " + @candidate.sec_name
         if current_user.admin?
