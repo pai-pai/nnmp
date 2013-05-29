d5281f8adde7e3a72cd7e3da6e7fa71bc033ff99
pai-pai <paimai.dororo@gmail.com>
2013-05-29 14:51:28 +0400

    Edit dashboard main page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -41,9 +41,10 @@ function showComment(object, x, y) {
 
 function showBaloon(object, x, y) {
     $("#comment").css({ 'top' : (-$(this).height()), 'left' : (-$(this).width) }).show();
-    console.log("document height: " + $(document).height() + "\r\nbaloon height: " + $(".comment-baloon").height() + "\r\ny: " + y);
-    if ($(document).height() < ( $(".comment-baloon").height() + y )) { 
-        y = y - ( $(".comment-baloon").height() - 50 ) 
+    console.log("document height: " + $(document).scrollTop() + "\r\nbaloon height: " + $(".comment-baloon").height() + "\r\ny: " + y);
+    if ($(document).scrollTop() < ( $(".comment-baloon").height() + y )) { 
+        y = y - ( $(".comment-baloon").height() - 60 );
+        $(".arrow").css( "top", $(".comment-baloon").height() - 30 );
     };
     console.log(y);
     $("#comment").css({ 'top' : y - 30, 'left' : x + 5 }).show();
