3e49e714a0d465c506713f4ff7790ecee9a513bf
pai-pai <paimai.dororo@gmail.com>
2013-06-10 14:56:05 +0400

    Edit changing top number

--- app/controllers/dashboard/pages_controller.rb
+++ app/controllers/dashboard/pages_controller.rb
@@ -5,11 +5,10 @@ class Dashboard::PagesController < ApplicationController
     layout "dashboard"
 
     def home
+        expire_action(:controller => '/dashboard/pages', :action => 'home')
         @nominations = Nomination.order("name").all
-        #@top_now = params[:top].to_i || 10
-        #@top = @top_now == 10 ? 5 : 10
-        @top = 5
-        @top_now = 10
+        @top_now = params[:top].to_i || 10
+        @top = @top_now == 10 ? 5 : 10
         respond_to do |format|
             format.html
             format.xls { response.headers['Content-Disposition'] = "attachment; filename=\"#{t('shared.excel.book_names.nominations')}.xls\"" }
