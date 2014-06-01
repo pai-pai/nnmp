b371d9c463ecbdf37d07d56e996a6180b9544afe
pai-pai <paimai.dororo@gmail.com>
2014-06-01 22:15:46 +0400

    Bug XXX - fix orgs top

--- app/models/org.rb
+++ app/models/org.rb
@@ -7,23 +7,23 @@ class Org < ActiveRecord::Base
     has_many :candidates
     belongs_to :area
 
-    def top_candidates( *num )
-        if !num.blank?
-            num = 5 if !num.is_a? Integer
-            Candidate.find_by_sql "SELECT c.id, c.fam_name, c.first_name, c.sec_name, c.org_id
-                                   FROM candidates c
-                                   LEFT JOIN votes v ON v.candidate_id=c.id
-                                   WHERE c.org_id=#{self.id}
-                                   GROUP BY c.id
-                                   ORDER BY COUNT(v.id) DESC
-                                   LIMIT #{num}"
-        else
-            Candidate.find_by_sql "SELECT c.id, c.fam_name, c.first_name, c.sec_name, c.org_id
-                                   FROM candidates c
-                                   LEFT JOIN votes v ON v.candidate_id=c.id
-                                   WHERE c.org_id=#{self.id}
-                                   GROUP BY c.id
-                                   ORDER BY COUNT(v.id) DESC"
+    def top_candidates(num)
+        candidates = self.candidates.select(
+            "candidates.id, candidates.fam_name,
+            candidates.first_name, candidates.sec_name,
+            candidates.org_id,
+            count(votes.id) AS votes_count, count(CASE
+                                                    WHEN votes.updated_at >= '#{Time.now.beginning_of_year}' THEN 1
+                                                    ELSE NULL
+                                                  END) AS votes_count_this_year").
+            joins(:votes).
+            where("votes.updated_at >= '#{Time.now.beginning_of_year}'").
+            group("candidates.id").
+            order("votes_count DESC")
+        limit = num
+        if num.blank? or !num.is_a? Integer
+            candidates = candidates.limit(limit)
         end
+        return candidates
     end
 end
